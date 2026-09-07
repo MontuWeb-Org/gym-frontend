ebd41032850abc52be5ec4cf29badef54a808e45
Mostafa Hassan <moustafahassan028@gmail.com>
2026-09-07 04:34:20 +0300

    feat: add auth service and types for user authentication and registration

---
+++ src/features/auth/index.ts
@@ -0,0 +1,5 @@
+export { default as authReducer } from "./store/auth.slice";
+export * from "./store/auth.slice";
+export { default as SignupView } from "./views/TrainerSignupView";
+export { authService } from "./services/auth.service";
+export * from "./types/auth.types";
