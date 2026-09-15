9558095eb53ddd76b3f90b4cbd5a7afbdfe93716
Mostafa Hassan <moustafahassan028@gmail.com>
2026-09-16 01:06:33 +0300

    feat: add user profile update handlers and service

--- src/mock-server/handlers/index.ts
+++ src/mock-server/handlers/index.ts
@@ -1,8 +1,10 @@
 import { authHandlers } from "./auth.handlers";
 import { trainerHandlers } from "./trainer.handlers";
+import {userHandlers} from "./user.handlers";
 
 
 export const handlers = [
   ...authHandlers,
   ...trainerHandlers,
+  ...userHandlers
 ];
