cfe306b83d5752ef6c31affadaf83a37126717d8
FatmaAbdou <your-email@example.com>
2026-09-19 01:52:14 +0300

    fixing lint and build bugs

--- src/features/user/store/user.slice.ts
+++ src/features/user/store/user.slice.ts
@@ -90,4 +90,6 @@ const userSlice = createSlice({
 export const { clearError, clearUser } = userSlice.actions;
 export default userSlice.reducer;
 
-export const selectCurrentUser = (state: { user: UserState }) => state.user.user;
+export const selectCurrentUser = (state: {
+  user: UserState;
+}) => state.user.user;
